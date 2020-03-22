cf47c2315ce9495c1fa891ccaa54ff8764ba973a
Ville Verkkonen <ville.verkkonen@solidabis.com>
2020-03-23 00:14:32 +0200

    Date refactoring works with Safari now

--- src/helpers/HelperFunctions.js
+++ src/helpers/HelperFunctions.js
@@ -1,5 +1,5 @@
 export const formatDate = dateString => {
-  const date = new Date(dateString)
+  const date = new Date(dateString.replace(/-/g, "/"))
   // Month index starts at 0 so add 1
   return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear()
 }
