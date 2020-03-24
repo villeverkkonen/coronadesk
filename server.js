1ff01f06ac582555f656d0a314837cc124910839
Ville Verkkonen <ville.verkkonen@solidabis.com>
2020-03-24 22:30:36 +0200

    Heroku configs for dependencies and server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express')
-const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
-app.use(express.static('build'))
-// app.use(express.static(path.join('src', 'build')))
-// app.get('/', (req, res) => {
-//   return res.sendFile(path.join('src', 'build', 'index.html'))
-// })
 
+app.use(express.static('build'))
 app.listen(port)
