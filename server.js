95815de44148911322933fac8090dc31583263c1
Ville Verkkonen <ville.verkkonen@solidabis.com>
2020-03-24 22:24:54 +0200

    Fixed path to build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
-app.use(express.static('src/build'))
+app.use(express.static('build'))
 // app.use(express.static(path.join('src', 'build')))
 // app.get('/', (req, res) => {
 //   return res.sendFile(path.join('src', 'build', 'index.html'))
