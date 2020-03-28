25671367a1a59f81c1515e9e4add7511eff4966d
Ville Verkkonen <ville.verkkonen@solidabis.com>
2020-03-29 01:22:31 +0200

    Node logs port it uses when started

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static('build'))
-app.listen(port)
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
